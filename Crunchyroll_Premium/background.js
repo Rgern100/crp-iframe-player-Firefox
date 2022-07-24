80a3be8daf5952a9fd9456ba50cf2a432e4aaed3
Louis_Soft <11809424+Rgern100@users.noreply.github.com>
2022-07-24 15:45:44 -0300

    Update background.js

--- Crunchyroll_Premium/background.js
+++ Crunchyroll_Premium/background.js
@@ -1,13 +1,17 @@
-chrome.webRequest.onHeadersReceived.addListener(
+var extraInfoSpec = ['blocking', 'responseHeaders'];
+var filter = {
+    urls: ["*://rgern100.github.io/*", "*://*.crunchyroll.com/*", "*://pl.crunchyroll.com/*", "*://localhost/*", "*://*.vrv.co/*"],
+};
+
+browser.webRequest.onHeadersReceived.addListener(
     function(details) {
       const newHeader = {name: 'Access-Control-Allow-Origin', value: '*'};
       const responseHeaders = details.responseHeaders.concat(newHeader);
       return { responseHeaders };
     },
     {
-      urls: ["https://*.mateus7g.github.io/*", "https://pl.crunchyroll.com/*", "https://www.crunchyroll.com/*", "http://localhost/*"],
+      urls: ["*://rgern100.github.io/*", "*://*.crunchyroll.com/*", "*://pl.crunchyroll.com/*", "*://localhost/*", "*://*.vrv.co/*"],
       types : ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
     },
-    ["blocking","responseHeaders", "extraHeaders"]
+    ["blocking","responseHeaders"]
   )
-  
